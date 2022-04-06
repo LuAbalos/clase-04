62130e30108733cf84b452110152065a63b4a906
Lu Abalos <abaloslucia@gmail.com>
2022-04-05 23:42:08 -0300

    Initial commit

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react" //6.8K (gzipped:2.7K);
+
+const ItemCount = () => {
+    const [contador, setContador] = useState(1);
+
+    const increment = () => {
+        if (contador < 5) {
+            setContador(contador+1)
+        }
+    }
+
+    const decrement = () => {
+        if (contador > 1){
+            setContador(contador-1)
+        }
+    }
+        return (
+            <div>
+                <div className='button-container'>
+                    <button onClick={decrement}> - </button>
+                    {contador}
+                    <button onClick={increment}> + </button>
+                    <button> Agregar al carrito </button>
+                </div>
+            </div>
+        )
+}
+
+export default ItemCount
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemCount from "./ItemCount";
+
+const ItemListContainer= () => {
+    return (
+      <div>
+          <ItemCount stock ="5" initial="0" />
+      </div>
+    );
+  }
+  
+  export default ItemListContainer;
+
+ 
