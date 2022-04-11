8f1ae86f16a42dbab913d430561e4cd0845f8774
Lu Abalos <abaloslucia@gmail.com>
2022-04-11 16:31:12 -0300

    Add function onAdd

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from "react" //6.8K (gzipped:2.7K);
+import ItemListContainer from "./ItemListContainer";
 
-const ItemCount = () => {
+const ItemCount = ({initial,stock,onAdd}) => {
     const [contador, setContador] = useState( 1 );
+    
 
     const increment = () => {
         if (contador < 5 ) {
@@ -20,10 +22,16 @@ const ItemCount = () => {
                     <button onClick={decrement} className='buttons-counters'> - </button>
                     {contador}
                     <button onClick={increment} className='buttons-counters'> + </button>
-                    <button className='buttons-counters' > Agregar al carrito </button>
+                    <button className='buttons-counters' onAdd={useState} > Agregar al carrito </button>
                 </div>
             </div>
         )
 }
 
 export default ItemCount
+
+//En el componente ItemListContainer debe estar definida la funcion onAdd y pasársela por props al componente ItemCount . 
+//El componente ItemCount debe recibir 3 props (initial, stock,onAdd).
+//Tenes que definir las props en el componente ItemCount en la linea 3 . Por ejemplo: 
+//const ItemCount = ({initial,stock,onAdd}) =>{
+//Finalmente cada vez que se aprete el botón agregar al carrito se debe llamar a la función onAdd() pasando la cantidad a consologuear o mostrar en un alert con ese valor . 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import ItemCount from "./ItemCount";
+import { useState } from "react";
+
+let num= useState
+
+function onAdd () {
+  if (num > 0) { 
+    console.log (num)
+ } else { 
+    
+ }
+  return console.log(onAdd);
+}
+
+
 
 const ItemListContainer= () => {
     return (
@@ -11,4 +25,6 @@ const ItemListContainer= () => {
   
   export default ItemListContainer;
 
- 
+ //En el componente ItemListContainer debe estar definida la funcion onAdd y pasársela por props al componente ItemCount . 
+
+//Finalmente cada vez que se aprete el botón agregar al carrito se debe llamar a la función onAdd() pasando la cantidad a consologuear o mostrar en un alert con ese valor . 
